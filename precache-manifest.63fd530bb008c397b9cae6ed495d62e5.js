d3728d9509a2113879545aaedaebb3de8fb1ca02
LaukeyMwaura <laukeymwaura@gmail.com>
2020-06-06 18:29:08 +0300

    Updates

---
+++ precache-manifest.63fd530bb008c397b9cae6ed495d62e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7658b449b385f6f7303ea415e8158ae1",
+    "url": "/todo-try-react/index.html"
+  },
+  {
+    "revision": "8be37ab1bf8c1791682c",
+    "url": "/todo-try-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "25172c1a720781e9ccfb",
+    "url": "/todo-try-react/static/css/main.ed5464a5.chunk.css"
+  },
+  {
+    "revision": "8be37ab1bf8c1791682c",
+    "url": "/todo-try-react/static/js/2.2fa6c41f.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/todo-try-react/static/js/2.2fa6c41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25172c1a720781e9ccfb",
+    "url": "/todo-try-react/static/js/main.ff8eb897.chunk.js"
+  },
+  {
+    "revision": "7c327f28c6463ef45176",
+    "url": "/todo-try-react/static/js/runtime-main.2830f42a.js"
+  },
+  {
+    "revision": "3b20766d314172faee87bcbba01605af",
+    "url": "/todo-try-react/static/media/back.3b20766d.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-try-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
